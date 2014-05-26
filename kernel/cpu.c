e9365c8869294d75b5116be00ed7bbae31392a9e
spock1104 <spock1104@gmail.com>
2014-05-26 09:45:44 -0400

    remove __cpuinit: find -name "*.c" | xargs sed -i "s/ __cpuinitdata//g" find -name "*.c" | xargs sed -i "s/ __cpuinit / /g" find -name "*.c" | xargs sed -i "s/ __cpuexit / /g" find -name "*.c" | xargs sed -i "s/ __cpuinit$//g" find arch/ -name "*.h" | xargs sed -i "s/ __cpuinit//g" find -name "*.c" | xargs sed -i "s/^__cpuinit //g" plus a few manual removals

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -290,7 +290,7 @@ EXPORT_SYMBOL(cpu_down);
 #endif /*CONFIG_HOTPLUG_CPU*/
 
 /* Requires cpu_add_remove_lock to be held */
-static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
+static int _cpu_up(unsigned int cpu, int tasks_frozen)
 {
 	int ret, nr_calls = 0;
 	void *hcpu = (void *)(long)cpu;
@@ -325,7 +325,7 @@ out_notify:
 	return ret;
 }
 
-int __cpuinit cpu_up(unsigned int cpu)
+int cpu_up(unsigned int cpu)
 {
 	int err = 0;
 
@@ -384,6 +384,7 @@ EXPORT_SYMBOL_GPL(cpu_up);
 #ifdef CONFIG_PM_SLEEP_SMP
 static cpumask_var_t frozen_cpus;
 
+#if !defined(CONFIG_MSM_MPDEC)
 void __weak arch_disable_nonboot_cpus_begin(void)
 {
 }
@@ -469,6 +470,7 @@ void __ref enable_nonboot_cpus(void)
 out:
 	cpu_maps_update_done();
 }
+#endif
 
 static int __init alloc_frozen_cpus(void)
 {
@@ -560,7 +562,7 @@ core_initcall(cpu_hotplug_pm_sync_init);
  * It must be called by the arch code on the new cpu, before the new cpu
  * enables interrupts and before the "boot" cpu returns from __cpu_up().
  */
-void __cpuinit notify_cpu_starting(unsigned int cpu)
+void notify_cpu_starting(unsigned int cpu)
 {
 	unsigned long val = CPU_STARTING;
 
