7a1afa12d170a4a605423069f71385d09f44550d
phenomx4 <phenomx49850@gmail.com>
2014-06-13 12:51:42 -0700

    Fix voice calls
    Fixes audio during voice calls.

--- sound/soc/msm/qdsp6/q6voice.h
+++ sound/soc/msm/qdsp6/q6voice.h
@@ -1009,7 +1009,13 @@ uint8_t voc_get_route_flag(uint16_t session_id, uint8_t path_dir);
 #define VOICE_SESSION_NAME "Voice session"
 #define VOIP_SESSION_NAME "VoIP session"
 #define VOLTE_SESSION_NAME "VoLTE session"
-#define SGLTE_SESSION_NAME "SGLTE session"
+#define VOICE2_SESSION_NAME "Voice2 session"
+
+#define VOC_PATH_PASSIVE 0
+#define VOC_PATH_FULL 1
+#define VOC_PATH_VOLTE_PASSIVE 2
+#define VOC_PATH_VOICE2_PASSIVE 3
+
 uint16_t voc_get_session_id(char *name);
 
 int voc_start_playback(uint32_t set);
