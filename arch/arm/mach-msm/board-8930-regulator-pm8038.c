bfeaf5116346469dd8d3e9556b7e936990081170
spock1104 <spock1104@gmail.com>
2015-01-24 01:44:33 -0500

    OC voltage fixes based on grarak

--- arch/arm/mach-msm/board-8930-regulator-pm8038.c
+++ arch/arm/mach-msm/board-8930-regulator-pm8038.c
@@ -471,9 +471,9 @@ msm8930_pm8038_gpio_regulator_pdata[] __devinitdata = {
 /* SAW regulator constraints */
 struct regulator_init_data msm8930_pm8038_saw_regulator_core0_pdata =
 	/*	      ID  vreg_name	       min_uV   max_uV */
-	SAW_VREG_INIT(S5, "8038_s5",	       850000, 1300000);
+	SAW_VREG_INIT(S5, "8038_s5",	       850000, 1400000);
 struct regulator_init_data msm8930_pm8038_saw_regulator_core1_pdata =
-	SAW_VREG_INIT(S6, "8038_s6",	       850000, 1300000);
+	SAW_VREG_INIT(S6, "8038_s6",	       850000, 1400000);
 
 /* PM8038 regulator constraints */
 struct pm8xxx_regulator_platform_data
